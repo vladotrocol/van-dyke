5b7e33af9dccb23ed8cd180f27264b9f20c32d54
Vlad <vladotrocol@yahoo.com>
2012-11-17 17:19:48 +0000

    Fixed conflicts in controller

--- Project/Assets/_Scripts/SimplePlayerController.js
+++ Project/Assets/_Scripts/SimplePlayerController.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD:Project/GitTest/Assets/_Scripts/SimplePlayerController.js
-#pragma strict
-
-function Start () {
-
-}
-
-function Update () {
-	if(Input.GetKey(KeyCode.UpArrow)) {
-		transform.Translate (0.0f, 0.0f, Time.deltaTime*5.0f);
-		}
-		else if(Input.GetKey(KeyCode.DownArrow)) {
-		transform.Translate (0.0f, 0.0f, Time.deltaTime*-5.0f);
-		}
-		else if(Input.GetKey(KeyCode.RightArrow)) {
-		transform.Translate (Time.deltaTime*8.0f, 0.0f, 0.0f);
-		}
-		else if(Input.GetKey(KeyCode.LeftArrow)) {
-		transform.Translate (-Time.deltaTime*8.0f, 0.0f, -0.0f);
-		}
-=======
 #pragma strict
 
 function Start () {
@@ -38,5 +17,4 @@ function Update () {
 	else if(Input.GetKey(KeyCode.RightArrow)){
 		transform.Rotate(0.0f, Time.deltaTime*80.0f, 0.0f);
 	}
->>>>>>> master:Project/Assets/_Scripts/SimplePlayerController.js
 }
