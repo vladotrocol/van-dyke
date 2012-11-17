898340e54aaacfbfc0f969dddb104185e4ecd0f6
Vlad <vladotrocol@yahoo.com>
2012-11-17 17:06:42 +0000

    Dynamic level phase 1

---
+++ Project/GitTest/Assets/_Scripts/Dynamic Levelling.js
@@ -0,0 +1,18 @@
+#pragma strict
+var sectionModel : GameObject;
+var player : GameObject;
+var playerStartPosition;
+var sectionModelSize;
+function Start () {
+	
+	var sectionInstance1 = Instantiate (sectionModel, Vector3(0.0f, 0.0f, 0.0f), transform.rotation);
+	sectionInstance1.AddComponent("MeshRenderer");
+	playerStartPosition = player.transform.position;
+	
+   	var size = sectionInstance1.renderer.bounds;
+	Debug.Log(size);
+}
+
+function Update () {
+
+}
