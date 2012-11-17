71263978a536845202972023ada289f0e45fb4ce
Joe Lewis <josephandrewlewis@gmail.com>
2012-11-17 16:48:45 +0000

    setting up

--- Project/GitTest/Assets/_Scripts/SimplePlayerController.js
+++ Project/GitTest/Assets/_Scripts/SimplePlayerController.js
@@ -5,16 +5,16 @@ function Start () {
 }
 
 function Update () {
-	if(Input.GetKey(KeyCode.UpArrow)){
-		transform.Translate(0.0f, 0.0f, Time.deltaTime*5.0f);
-	}
-	else if(Input.GetKey(KeyCode.DownArrow)){
-		transform.Translate(0.0f, 0.0f, -Time.deltaTime*5.0f);
-	}
-	if(Input.GetKey(KeyCode.LeftArrow)){
-		transform.Rotate(0.0f, -Time.deltaTime*80.0f, 0.0f);
-	}
-	else if(Input.GetKey(KeyCode.RightArrow)){
-		transform.Rotate(0.0f, Time.deltaTime*80.0f, 0.0f);
-	}
+	if(Input.GetKey(KeyCode.UpArrow)) {
+		transform.Translate (0.0f, 0.0f, Time.deltaTime*5.0f);
+		}
+		else if(Input.GetKey(KeyCode.DownArrow)) {
+		transform.Translate (0.0f, 0.0f, Time.deltaTime*-5.0f);
+		}
+		else if(Input.GetKey(KeyCode.RightArrow)) {
+		transform.Translate (Time.deltaTime*8.0f, 0.0f, 0.0f);
+		}
+		else if(Input.GetKey(KeyCode.LeftArrow)) {
+		transform.Translate (-Time.deltaTime*8.0f, 0.0f, -0.0f);
+		}
 }
