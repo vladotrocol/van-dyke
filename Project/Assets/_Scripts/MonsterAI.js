a047c93784492f702f82412c518648f07e915f66
Matthew Bessey <bessey@gmail.com>
2012-11-25 15:27:24 +0000

    Added super basic monster AI. Deleted unneeded scenes.

--- Project/Assets/_Scripts/MonsterAI.js
+++ Project/Assets/_Scripts/MonsterAI.js
@@ -1,9 +1,12 @@
 #pragma strict
-
+private var player : GameObject;
 function Start () {
-
+	player = GameObject.Find("Player");
+	transform.position = player.transform.position;
+	transform.position.z = transform.position.z - 2;
 }
 
 function Update () {
-
+	rigidbody.velocity = player.rigidbody.velocity;
+	transform.position.y = 1; // FIXME: this stops the monster falling through the floor, but they shouldn't.
 }
