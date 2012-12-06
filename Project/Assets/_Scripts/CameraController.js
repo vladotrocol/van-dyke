b4195c2b16ea537fd75a6deb31be7fd303778156
Vlad <vladotrocol@yahoo.com>
2012-12-06 17:51:32 +0000

    Removing unneeded meta files, and apparently adding some meta files that unity has deemed fit.

--- Project/Assets/_Scripts/CameraController.js
+++ Project/Assets/_Scripts/CameraController.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-#pragma strict
-
-var Play : GameObject;
-var startPosz : float;
-var startPosx : float;
-var startPosy : float;
-var curPosz : float;
-var curPosy: float;
-var curPosx: float;
-
-
-function Start () {
-	Play = GameObject.FindGameObjectWithTag("Player");
-	startPosz = Play.transform.position.z;
-	startPosy = Play.transform.position.y;
-	startPosx = Play.transform.position.x;
-}
-
-function Update () {
-	curPosz = Play.transform.position.z;
-	curPosy = Play.transform.position.y;
-	curPosx = Play.transform.position.x;
-	
-	if (curPosz != startPosz)
-	{
-	//					Side, Up   , forward
-	transform.Translate(0.0f, 0.0f, (curPosz - startPosz));
-	startPosz = curPosz;
-	}
-	
-	if (curPosy != startPosy)
-	{
-	//					Side,  Up                  , forward
-	transform.Translate(0.0f, (curPosy - startPosy), 0.0f );
-	startPosy = curPosy;
-	}
-	if (curPosx != startPosx)
-	{
-	//					Side,                 Up   , forward
-	transform.Translate((curPosx - startPosx), 0.0f, 0.0f);
-	startPosx = curPosx;
-	} 
-=======
 #pragma strict
 // player and camera
 var Play : GameObject;
@@ -117,5 +73,4 @@ function Update () {
 		transform.Translate((curPosx - startPosx ), 0.0f, 0.0f);
 		startPosx = curPosx;
 	}
->>>>>>> 9088ff8b2355542afdf53fa85864b8bd86af2371
 }
